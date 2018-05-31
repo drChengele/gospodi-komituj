a3a472c05a18beccff5111a5e31834ca3e2f95be
EnDotter <mojskutere@gmail.com>
2018-05-31 18:30:43 +0200

    nova scena sa nekim prostim kretanjem

---
+++ Assets/Scripts/BadCharacterController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BadCharacterController : MonoBehaviour {
+
+    public float playerSpeed = 10;
+    private Vector3 playerVelocity;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        playerVelocity = Vector3.Normalize(new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"))) * playerSpeed * Time.deltaTime;
+        transform.position += playerVelocity;
+        playerVelocity = Vector3.zero;
+	}
+}
