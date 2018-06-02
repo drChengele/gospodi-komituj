ee957603647da11128d5e7aa4557c218762d1d66
EnDotter <mojskutere@gmail.com>
2018-06-02 16:23:26 +0200

    object spawner, i osnova za asteroid behaviour

--- Assets/Code/VolumetricObjectGenerator.cs
+++ Assets/Code/VolumetricObjectGenerator.cs
@@ -4,15 +4,76 @@ using UnityEngine;
 
 public class VolumetricObjectGenerator : MonoBehaviour {
 
-    public GameObject[] spawnableObjects;
+    public enum SpawnObjectType
+    {
+        Asteroid,
+        Debris,
+        Resources,
+        RandomStuff
+    }
 
+    public SpawnerData[] spawnerData;
 
-	// Use this for initialization
-	void Start () {
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public Vector3 volumeSize;
+
+    private Vector3 minCorner;
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        for (int i = 0; i < spawnerData.Length; i++)
+        {
+            if (spawnerData[i].canSpawn)
+            {
+                if (spawnerData[i].currentTime >= spawnerData[i].cooldown)
+                {
+                    spawnerData[i].currentTime = 0f;
+                    this.GenerateObject(spawnerData[i]);
+                }
+                else spawnerData[i].currentTime += Time.deltaTime;
+            }
+
+        }
+    }
+
+    void GenerateObject(SpawnerData data)
+    {
+        var objectToSpawn = data.ObjectToSpawn();
+
+        var spawnPosition = new Vector3(transform.position.x + Random.Range(-volumeSize.x / 2, volumeSize.x / 2),
+                                    transform.position.y + Random.Range(-volumeSize.y / 2, volumeSize.y / 2),
+                                    transform.position.z + Random.Range(-volumeSize.z / 2, volumeSize.z / 2));
+        Instantiate(objectToSpawn, spawnPosition, Quaternion.Euler(Random.Range(0,360), Random.Range(0, 360), Random.Range(0, 360)));
+        objectToSpawn.transform.localScale = new Vector3(data.CalculateScale(), data.CalculateScale(), data.CalculateScale());
+    }
+
+    [System.Serializable]
+    public class SpawnerData {
+        public string name;
+        
+        public SpawnObjectType type;
+        public bool canSpawn = true;
+        [HideInInspector]
+        public float currentTime = 0f;
+        public float cooldown = 0.5f;
+        public GameObject[] objectsToSpawn;
+        [Range(0.01f, 10f)]
+        public float minScale;
+        [Range(0.5f, 100f)]
+        public float maxScale;
+
+        public GameObject ObjectToSpawn()
+        {
+            return objectsToSpawn[Random.Range(0, objectsToSpawn.Length)];
+        }
+
+        public float CalculateScale()
+        {
+            return Random.Range(minScale, maxScale);
+        }
+    }
 }
---
+++ Assets/SpawnablePrefabs/AsteroidBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidBehaviour : MonoBehaviour {
+
+    private Vector3 rotationVector;
+    private float rotationSpeed;
+	// Use this for initialization
+	void Awake () {
+        rotationSpeed = Random.Range(1f, 100f);
+        rotationVector = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SelfSpinner();
+	}
+
+    void SelfSpinner()
+    {
+        transform.Rotate(rotationVector * rotationSpeed * Time.deltaTime);
+    }
+
+    void KillMe()
+    {
+        Destroy(this.gameObject);
+    }
+}
