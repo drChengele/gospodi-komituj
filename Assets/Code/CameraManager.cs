40afeeb87040d5373ad7873f63b9b01ea32ad30a
Goran <drChengele@gmail.com>
2018-06-02 13:21:10 +0200

    Triple camera setup, some effects, some systems code.

---
+++ Assets/Code/CameraManager.cs
@@ -0,0 +1,9 @@
+﻿
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour {
+    public Camera pilotCamera;
+    public Camera engineerCamera;
+    public Camera outsideCamera;
+       
+}
---
+++ Assets/Code/Logic/ShipSystem.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+
+public abstract class ShipSystem : MonoBehaviour {
+
+    public event Action<ShipSystem> SystemHasTooMuchEnergy;
+    public event Action<ShipSystem> SystemIsOutOfEnergy;
+
+    public PilotController controller => myShip.pilotController;
+
+    public virtual void Initialize(ShipSystems myship) {
+        myShip = myship;
+    }
+
+    public ShipSystems myShip { get; private set; }
+    public float CurrentEnergy { get; private set; }
+
+    const float MinEnergy = 0f;
+    const float MaxEnergy = 100f;
+
+    public void TryChangeCurrentEnergy(float delta) {
+        this.CurrentEnergy += delta;
+        if (this.CurrentEnergy < MinEnergy && delta < 0f) { // tried to draw energy but could not
+            OutOfEnergy();
+        }
+
+        if (this.CurrentEnergy > MaxEnergy && delta > 0f) { // tried to fill energy but could 
+            TooMuchEnergy();
+        }
+    }
+
+    protected virtual void TooMuchEnergy() {
+        SystemHasTooMuchEnergy?.Invoke(this);
+    }
+
+    protected virtual void OutOfEnergy() {
+        SystemIsOutOfEnergy?.Invoke(this);
+    }
+
+    internal virtual void UpdateLogic() { }
+}
