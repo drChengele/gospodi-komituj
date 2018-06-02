b54cb2c0bbdbcc38a327900020e064540d798792
Goran <drChengele@gmail.com>
2018-06-03 00:46:29 +0200

    The entire game

--- Assets/Code/CharacterController/JamShipController.cs
+++ Assets/Code/CharacterController/JamShipController.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 public interface IInertiaProvider {
     Vector3 CurrentVelocity { get; }
     Vector3 CurrentAccelerationRelative { get; }
+    Vector3 CurrentAccelerationRelativeZeroToOne { get; }
 }
 
 public class JamShipController : MonoBehaviour, IEngineEffector, IInertiaProvider {
     public ShipSystems systems;
 
-    [Range(0f, 10f)]
-    public float thrustVertical;
+    [Range(2f, 60f)]
+    public float thrustPower;
 
-    [Range(0f, 10f)]
-    public float thrustTransversal;
+    [Range(20f, 600f)]
+    public float rollPower;
 
     public float forwardSpeed;
 
@@ -22,13 +23,17 @@ public class JamShipController : MonoBehaviour, IEngineEffector, IInertiaProvide
 
     public Vector3 CurrentVelocity => currentVelocity;
     public Vector3 CurrentAccelerationRelative => activeAcceleration;
+    public Vector3 CurrentAccelerationRelativeZeroToOne => activeAccelNormalized;
 
     Vector3 currentVelocity;
-
     Vector3 activeAcceleration;
+    Vector3 activeAccelNormalized;
+
+    float currentRoll;
     
     void Update () {
         ApplyVelocity();
+        UpdateRollTation();
 	}
 
     private void FixedUpdate() {
@@ -38,18 +43,32 @@ public class JamShipController : MonoBehaviour, IEngineEffector, IInertiaProvide
     private void ApplyIntertiaAndFriction() {
         currentVelocity.x *= inertiaRetain;
         currentVelocity.y *= inertiaRetain;
+        //currentRoll *= inertiaRetain;
     }
 
-    
     public void ApplyThrust(Vector2 thrust) {
-        thrust.Scale(new Vector2(thrustTransversal, thrustVertical));
-        activeAcceleration = thrust;
-        currentVelocity += activeAcceleration * Time.fixedDeltaTime;
+        activeAccelNormalized = thrust;
+        activeAcceleration = thrust * this.thrustPower;
+
+        var transformedVector = transform.TransformVector(activeAcceleration); // take rotation into account
+
+        currentVelocity += transformedVector * Time.fixedDeltaTime;
     }
 
     void ApplyVelocity() {
         FixVelocity();
         transform.position += CurrentVelocity * Time.deltaTime;
+    }
+
+    void UpdateRollTation() {
+        currentRoll = Mathf.MoveTowards(currentRoll, targetRoll, rollPower * Time.deltaTime);
+        transform.Rotate(0, 0, currentRoll * Time.deltaTime, Space.Self);
+    }
+
+    float targetRoll;
+
+    public void ApplyRoll(float roll) {
+        targetRoll = roll * 120f;
     }
 
     void FixVelocity() {
--- Assets/Code/Logic/PilotController.cs
+++ Assets/Code/Logic/PilotController.cs
@@ -3,11 +3,13 @@
 // processes input and communicates the signals
 public class PilotController : MonoBehaviour {
     internal Vector2 thrusterSignal;
+    internal float rollSignal;
     internal bool    fireSignal;
     internal bool    shieldSignal;
 
     private void Update() {
         thrusterSignal = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        rollSignal = Input.GetAxis("Roll");        
         if (thrusterSignal.magnitude > 1f) thrusterSignal.Normalize();
         fireSignal = Input.GetButton("Fire");
         shieldSignal = Input.GetButton("Shield");
