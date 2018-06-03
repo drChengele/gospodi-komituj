9b117afcddab37fa04de83d6fea150175bf7da1e
EnDotter <mojskutere@gmail.com>
2018-06-03 10:30:37 +0200

    asdsad

---
+++ Assets/EndTextManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndTextManager : MonoBehaviour {
+
+    public Text won;
+
+    public Text lost;
+
+    public bool gameWon = false;
+
+    private void Awake()
+    {
+        DisplayText();
+    }
+
+    public void DisplayText()
+    {
+        if (gameWon)
+        {
+            won.gameObject.SetActive(true);
+            lost.gameObject.SetActive(false);
+        }
+        else
+        {
+            lost.gameObject.SetActive(true);
+            won.gameObject.SetActive(false);
+        }
+    }
+
+}
