daabb9e66701eba34432e5f48ac42965c67e962e
EnDotter <mojskutere@gmail.com>
2018-06-03 09:12:06 +0200

    menu i tranzicija na prvu scenu

---
+++ Assets/GlowToggler.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlowToggler : MonoBehaviour {
+
+    public void ToggleVisibility()
+    {
+        if (gameObject.activeInHierarchy) gameObject.SetActive(false);
+        else gameObject.SetActive(true);
+    }
+}
